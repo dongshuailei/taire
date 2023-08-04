6351e212c7f67ffcdda2cca3dcfcdc48dc5446d0
董帅磊 <2638290411@qq.com>
2023-08-04 14:21:30 +0800

    111

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button type="info">信息按钮</el-button>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
